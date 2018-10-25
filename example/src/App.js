a731d7ae21b91703601e33399e8845369423d653
Jonas Hackenberg <jonas@arkulpa.at>
2018-10-25 10:56:11 +0200

    make things prettier 💅

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
-/*  1. Import for material date range picker */
-import {DateRangePicker} from 'material-date-range-picker';
+/*  1. Import material date range picker */
+import { DateRangePicker } from 'material-date-range-picker';
 
 const Wrapper = styled.div`
   width: 350px;
@@ -10,20 +10,17 @@ const Wrapper = styled.div`
 `;
 
 class App extends Component {
-  /*  2. Add state handler */
-
   state = {
     fromDate: null,
     toDate: null
   };
-
+  /*  2. Add state handler */
   _handleDateRangeChange = update => this.setState(update);
 
   render() {
     return (
       <Wrapper>
         {/* 3. Add the material date range picker in your project */}
-
         <DateRangePicker
           fromDate={this.state.fromDate}
           toDate={this.state.toDate}
